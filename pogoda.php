d4928cac26fc2a65c62151a9ff80e0ff29e89c3a
xzvc <patryk.kantarek@wp.pl>
2024-12-02 14:23:58 +0100

    add form
    Signed-off-by: xzvc <patryk.kantarek@wp.pl>

--- pogoda.php
+++ pogoda.php
@@ -6,13 +6,20 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action='pogoda.php' method='get'>
+        <input type='text' name='location'/>
+        <input type='submit'>
+    </form>
 
 <?php
  $user_ip = $_SERVER['REMOTE_ADDR'];
  echo "Your IP address is: " . htmlspecialchars($user_ip);
  $location = json_decode(file_get_contents('http://ip-api.com/json/'.$user_ip), true);
+ if($_GET['location']) {
+    $city = $_GET['location'];
+ } else {
  $city = $location['city'];
+ }
  $weather_url = "http://api.openweathermap.org/data/2.5/weather?q=" . urlencode($city) . "&appid=9c36694cf729a55fe303b7e333375c56&units=metric";
  $weather_data = json_decode(file_get_contents($weather_url), true);
  echo "Current weather in " . htmlspecialchars($city) . ": " . htmlspecialchars($weather_data['weather'][0]['description']) . "<br>";
