e2c8952245c7ed5b70d18966dd4ec72b4d41e395
xzvc <patryk.kantarek@wp.pl>
2024-12-02 14:11:55 +0100

    current weather
    Signed-off-by: xzvc <patryk.kantarek@wp.pl>

--- pogoda.php
+++ pogoda.php
@@ -11,8 +11,14 @@
 <?php
  $user_ip = $_SERVER['REMOTE_ADDR'];
  echo "Your IP address is: " . htmlspecialchars($user_ip);
- $location = file_get_contents('http://ip-api.com/json/'.$user_ip.'?fields=city');
- echo $location;
+ $location = json_decode(file_get_contents('http://ip-api.com/json/'.$user_ip), true);
+ $city = $location['city'];
+ $weather_url = "http://api.openweathermap.org/data/2.5/weather?q=" . urlencode($city) . "&appid=9c36694cf729a55fe303b7e333375c56&units=metric";
+ $weather_data = json_decode(file_get_contents($weather_url), true);
+ echo "Current weather in " . htmlspecialchars($city) . ": " . htmlspecialchars($weather_data['weather'][0]['description']) . "<br>";
+ echo "Temperature: " . htmlspecialchars($weather_data['main']['temp']) . "°C<br>";
+ echo "Humidity: " . htmlspecialchars($weather_data['main']['humidity']) . "%<br>";
+ echo "Cloudiness: " . htmlspecialchars($weather_data['clouds']['all']) . "%<br>";
 ?>
 </body>
 </html>
